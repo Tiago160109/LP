cd9926b8709b6b11d2d22fcd37327289860bb905
Samuel <Samuel@DESKTOP-63TOBIF>
2025-05-18 22:04:10 -0300

    Atividade LAboratorio

---
+++ LaboratorioLP/src/main/java/br/cefetmg/inf/lista01_1000/Main.java
@@ -0,0 +1,12 @@
+
+package br.cefetmg.inf.lista01_1000;
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+
+    }
+ 
+}
