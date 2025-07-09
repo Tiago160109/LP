e9fe035aa1a2efeff906d6290a9e8b3c51bec28c
Tiago160109 <tiagopereira16012009@gmail.com>
2025-07-09 13:45:52 -0300

    finalizando o lab20250707.p1110

--- LaboratorioLP/src/main/java/br/cefetmg/inf/lab20250707/p1110/Main.java
+++ LaboratorioLP/src/main/java/br/cefetmg/inf/lab20250707/p1110/Main.java
@@ -1,27 +1,29 @@
 package br.cefetmg.inf.lab20250707.p1110;
 import java.io.IOException;
-import java.util.Scanner;
-class Baralho {
-    private Carta inicio;
-    private Carta fim;
+import java.util.*;
+class DequeEncadeado {
+    private No inicio;
+    private No fim;
     private int tam;
     
-    Baralho() {
+    private class No {
+        int valor;
+        No proximo;
+        No anterior;
+
+        No(int valor) {
+            this.valor = valor;
+        }
+    }
+    
+    DequeEncadeado() {
         inicio = null;
         fim = null;
         tam = 0;
     }
-    class Carta {
-        int valor;
-        Carta proximo;
-        Carta anterior;
-
-        Carta(int valor) {
-            this.valor = valor;
-        }
-    }
+    
     public void inserirNoInicio(int item) {
-        Carta novo = new Carta(item);
+        No novo = new No(item);
         if(estaVazia()) {            
             inicio = novo;
             fim = novo;
@@ -34,7 +36,7 @@ class Baralho {
         tam++;
     }
     public void inserirNoFim(int item) {
-        Carta novo = new Carta(item);
+        No novo = new No(item);
         if(estaVazia()) {
             inicio = novo;
             fim = novo;
@@ -95,26 +97,61 @@ class Baralho {
     public int tamanho() {
         return tam;
     }
-    Baralho(int totalDeCartas){
-        inicio = null;
-        fim = null;
-        for(int i = 0; i < totalDeCartas; i++){
-            if(totalDeCartas != 50){
-                inserirNoFim(i);
-            }
+    
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int n = scanner.nextInt();
+            if (n == 0) break;
+
+            Baralho simulador = new Baralho(n);
+            simulador.simular();
+            System.out.println(simulador.pegaCartasDescartadas());
+            System.out.println(simulador.pegaCartaRestante());
         }
     }
 }
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Baralho barai = new Baralho();
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+class Baralho {
+    private final int n;
+    private final DequeEncadeado deque;
+    private final DequeEncadeado descartadas;
 
-        while(barai.tamanho() > 1){
-            barai.removerPrimeiro();
-            int retiro = removerPrimeiro();
+    public Baralho(int n) {
+        this.n = n;
+        this.deque = new DequeEncadeado();
+        this.descartadas = new DequeEncadeado();
+    }
+    public void simular() {
+        for (int i = 1; i <= n; i++) {
+            deque.inserirNoFim(i);
         }
-        
+        while (deque.tamanho() > 1) {
+            int cartaDescartada = deque.removerPrimeiro();
+            descartadas.inserirNoFim(cartaDescartada); 
+
+            int cartaMover = deque.removerPrimeiro();
+            deque.inserirNoFim(cartaMover);
+        }
+    }
+    public String pegaCartasDescartadas() {
+    String saida = "Discarded cards:";
+
+    if (descartadas.estaVazia()) {
+        return saida;
+    }
+    saida += " ";
+    while (!descartadas.estaVazia()) {
+        saida += descartadas.removerPrimeiro();
+        if (!descartadas.estaVazia()) {
+            saida += ", ";
+        }
+    }
+    return saida;
+}
+    public String pegaCartaRestante() {
+        return "Remaining card: " + deque.obterPrimeiro();
     }
 }
